2f9a002d9b074058c00eff9a9308c64dbc7c5b21
jredfox <jredfox_@outlook.com>
2024-10-14 10:06:09 -0500

    Update DeAWTTransformer.java

--- src/main/java/jredfox/DeAWTTransformer.java
+++ src/main/java/jredfox/DeAWTTransformer.java
@@ -1,12 +1,11 @@
 package jredfox;
 
-import java.applet.Applet;
-import java.awt.Canvas;
-import java.awt.Component;
-import java.awt.Frame;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,7 +14,7 @@ import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
-import org.objectweb.asm.ClassReader;
+import org.apache.commons.io.FileUtils;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
@@ -26,14 +25,10 @@ import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.JumpInsnNode;
 import org.objectweb.asm.tree.LabelNode;
-import org.objectweb.asm.tree.LdcInsnNode;
-import org.objectweb.asm.tree.LineNumberNode;
-import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.TypeInsnNode;
 import org.objectweb.asm.tree.VarInsnNode;
 
-import jml.gamemodelib.GameModeLib;
 import jml.gamemodelib.GameModeLibAgent;
 
 /**
@@ -42,15 +37,16 @@ import jml.gamemodelib.GameModeLibAgent;
  * @credit MoreStack for suggesting editing {@link java.awt.Component#setVisible(boolean)} directly instead of whitelisting 50 classes
  */
 public class DeAWTTransformer implements ClassFileTransformer {
-
-	public byte[] component;
-	public static final String DEAWT_VERSION = "0.1";//TODO: make part of the filename
+	
+	public static File component = null;
 	
 	public static void init(Instrumentation inst)
 	{
 		if(GameModeLibAgent.hasForge && net.minecraftforge.common.ForgeVersion.getMajorVersion() < 8 && isCoreMod() && isDeAWT())
 		{
 			System.out.println("Registering Agent DeAWTTransformer");
+			component = new File(System.getProperty("user.dir"), "asm/cache/dpi-fix/java/awt/Component.class").getAbsoluteFile();
+			component.delete();//delete previous caches
 			inst.addTransformer(new DeAWTTransformer());
 			GameModeLibAgent.forName("java.awt.Component");//Force Load the java.awt.Frame Class
 		}
@@ -83,25 +79,30 @@ public class DeAWTTransformer implements ClassFileTransformer {
 		
 		if(className.equals("java/awt/Component"))
 		{
-			if(component != null)
-				return component;
+			System.out.println("Transforming java.awt.Component to prevent flashes");
+			
+			//Return the cached file if it exists
+			if(component.exists())
+				return toByteArray(component);
+			
 			try
 			{
-				System.out.println("Transforming java.awt.Component to prevent flashes");
 				ClassNode classNode = CoreUtils.getClassNode(classBytes);
-				
 				//add the field canSetVisible
 				classNode.fields.add(new FieldNode(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, "canSetVisible", "Z", null, null));
 				
+				//transform setVisible
 				MethodNode m = CoreUtils.getMethodNode(classNode, "setVisible", "(Z)V");
 				deawt(m, false);
 				
+				//transform show
 				MethodNode show = CoreUtils.getMethodNode(classNode, "show", "()V");
 				if(show != null)
 					deawt(show, true);
 				
-				this.component = CoreUtils.toByteArray(CoreUtils.getClassWriter(classNode, ClassWriter.COMPUTE_MAXS), className);
-				return this.component;
+				byte[] clazzBytes = CoreUtils.toByteArray(CoreUtils.getClassWriter(classNode, ClassWriter.COMPUTE_MAXS), className);
+				toFile(clazzBytes, component);
+				return clazzBytes;
 			}
 			catch(Throwable t)
 			{
@@ -144,6 +145,74 @@ public class DeAWTTransformer implements ClassFileTransformer {
 		l.add(l1);
 		l.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null));
 		m.instructions.insert(l);
+	}
+	
+	public static void toFile(byte[] data, File f)
+	{
+		f.getParentFile().mkdirs();
+		
+    	InputStream in = null;
+    	OutputStream out = null;
+    	try
+    	{
+    		in = new ByteArrayInputStream(data);
+    		out = new FileOutputStream(f);
+    		copy(in, out);
+    	}
+    	catch(Throwable e)
+    	{
+    		e.printStackTrace();
+    	}
+    	finally
+    	{
+    		closeQuietly(in);
+    		closeQuietly(out);
+    	}
+	}
+	
+    public static byte[] toByteArray(File file)
+    {
+    	InputStream input = null;
+        ByteArrayOutputStream output = null;
+        try
+        {
+        	input = new FileInputStream(file);
+        	output = new ByteArrayOutputStream();
+        	copy(input, output);
+        }
+        catch(Throwable e)
+        {
+        	e.printStackTrace();
+        }
+        finally
+        {
+            closeQuietly(input);
+            closeQuietly(output);
+        }
+        return output.toByteArray();
+    }
+    
+	public static void copy(InputStream in, OutputStream out) throws IOException
+	{
+		byte[] buffer = new byte[1048576/2];
+		int length;
+   	 	while ((length = in.read(buffer)) >= 0)
+		{
+			out.write(buffer, 0, length);
+		}
+	}
+	
+	public static void closeQuietly(Closeable clos)
+	{
+		try 
+		{
+			if(clos != null)
+				clos.close();
+		}
+		catch (IOException e)
+		{
+			
+		}
 	}
 
 }
